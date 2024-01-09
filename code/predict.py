5a626d7dc42661aa61b57225d7616548dafb89c1
tangguoling <83218093+tangguoling@users.noreply.github.com>
2024-01-09 11:57:20 +0800

    Add files via upload

--- code/predict.py
+++ code/predict.py
@@ -7,7 +7,8 @@ Created on Mon Jan  8 18:49:52 2024
 
 from core.predict import predict
 import yaml
-from config_training import configuration
+from config.config_training import configuration
+from config.config_predicting import configuration_predict
 if __name__ == '__main__':
     print('\nWellcome to use ADPT v1.1.0 for keypoints detection.')
     with open('config.yaml', 'r', encoding='utf-8') as f:
@@ -19,7 +20,6 @@ if __name__ == '__main__':
     IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts = configuration(result)
     # print(result)
     
-    from config_predicting import configuration_predict
     videos,save_video,model_path,colors,pcutoff = configuration_predict(result_predict)
     num_classes = 1
     stride = 8
