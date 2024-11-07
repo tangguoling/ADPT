b44a07fa59a8e9fa324de6632be80d926e8d3d2d
Guoling Tang <guoling_tang@163.com>
2024-11-07 17:25:31 +0800

    Add files via upload

--- code/predict.py
+++ code/predict.py
@@ -7,29 +7,31 @@ Created on Mon Jan  8 18:49:52 2024
 
 import warnings
 warnings.filterwarnings('ignore')
-from core.predict import predict
+from core.predict import predict, predict_picture
 import yaml
 from config.config_training import configuration
 from config.config_predicting import configuration_predict
 if __name__ == '__main__':
-    print('\nWellcome to use ADPT v1.1.1 for keypoints detection.')
-    with open('config.yaml', 'r', encoding='utf-8') as f:
+    json_file = 'config_singlemouse.yaml'
+    print('\nWellcome to use ADPT v1.2.1 for keypoints detection.')
+    with open(json_file, 'r', encoding='utf-8') as f:
         result = yaml.load(f.read(), Loader=yaml.FullLoader)
     with open('config_predict.yaml', 'r', encoding='utf-8') as f:
         result_predict = yaml.load(f.read(), Loader=yaml.FullLoader)
         
     # print('\nTraining configuration:')
-    IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts, early_stop = configuration(result)
+    IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts, early_stop,centre,num_classes = configuration(result)
     # print(result)
     
-    videos,save_video,model_path,colors,pcutoff = configuration_predict(result_predict)
-    num_classes = 1
+    videos,save_video,model_path,colors,pcutoff,scorer = configuration_predict(result_predict)
+    # centre = 4
+    # num_classes = 2 
     stride = 8
     evaluate = False # False
     save_path = '_' + str(shuffle_num)
     
     print('\nStart analyzing videos!\n')
-    predict(IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts,videos,save_video,model_path,colors,pcutoff)
+    predict(IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts,videos,save_video,model_path,colors,pcutoff,num_classes,scorer,centre)
     
     # for idx, bodypart in enumerate(bodyparts):
     #     print('RMSE (' + bodypart + '): ', model_rmse[1][idx])
--- code/train.py
+++ code/train.py
@@ -7,26 +7,33 @@ Created on Mon Jan  8 18:49:52 2024
 
 import warnings
 warnings.filterwarnings('ignore')
-from core.train import train
+from core.train_latest import train
 from core.data_aug import data_augmentation
 import yaml
 from config.config_training import configuration
+import numpy as np
 if __name__ == '__main__':
-    print('\nWellcome to use ADPT v1.1.1 for keypoints detection.')
+    json_file = 'config_singlemouse.yaml'
+    print('\nWellcome to use ADPT v1.2.1 for keypoints detection.')
     print('\nTraining configuration:')
-    with open('config.yaml', 'r', encoding='utf-8') as f:
+    with open(json_file, 'r', encoding='utf-8') as f:
         result = yaml.load(f.read(), Loader=yaml.FullLoader)
-    IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts, early_stop = configuration(result)
+    IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts, early_stop,centre, num_classes = configuration(result)
     print(result)
-    num_classes = 1
+    # centre = 4
+    # num_classes = 1
     stride = 8
-    evaluate = False # False
+    evaluate = False # T
+    # shuffle_num = 3
     save_path = '_' + str(shuffle_num)
-    
-    model_rmse = train('ADPT', save_path, IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts,data_augmentation(), early_stop, evaluate)
+    animal = 'singlemouse'
+    class_coe = [5,1,1]
+    model_rmse = train('ADPT',animal, save_path, IMG_SIZE_H_ori, IMG_SIZE_W_ori, global_scale, IMG_SIZE_H, IMG_SIZE_W, BATCH_SIZE, variation, delta, initial_learning_rate, alpha,EPOCHS, WARMUP_EPOCHS, NUM_KEYPOINT, NUM_KEYPOINTS, shuffle_num, TrainingFraction, Tranfer_LR, channels,IMG_DIR, JSON, kp_con, initial_weight, bodyparts,data_augmentation(),class_coe, early_stop, evaluate,num_classes,centre)
     
     for idx, bodypart in enumerate(bodyparts):
-        print('RMSE (' + bodypart + '): ', model_rmse[1][idx])
-    print('RMSE (average): ', model_rmse[1][:len(bodyparts)])
+        print('RMSE (' + bodypart + '): ', model_rmse[0][idx])
+    print(np.mean(model_rmse[0][:len(bodyparts)]))
+    # print('RMSE (average): ', model_rmse[1][:len(bodyparts)])
     
     print('\nNow the model has been trained, and you can start analyzing the videos!')
+    
